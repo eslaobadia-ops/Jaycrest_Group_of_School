37022371597fc0eae212984c0b2dd2aaca91628e
eslaobadia-ops <eslaobadia@gmail.com>
2025-12-18 03:57:23 +0100

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
+import axios from "axios";
 
-import axios from 'axios'
-export const api = axios.create({ baseURL: 'https://jaycrest1.onrender.com' })
-api.interceptors.request.use(c => {
-  const t = localStorage.getItem('token')
-  if (t) c.headers.Authorization = `Bearer ${t}`
-  return c
-})
+const api = axios.create({
+  baseURL: "https://jaycrest1.onrender.com",
+});
+
+export default api;
+
