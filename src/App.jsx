b7ee492ae70c04e78a2c387c6d2fecd217094c6a
eslaobadia-ops <eslaobadia@gmail.com>
2025-12-18 03:47:47 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Login from './pages/Login'
-import Register from './pages/Register'
-export default () => (
-<BrowserRouter>
-<Routes>
-<Route path="/" element={<Login />} />
-<Route path="/register" element={<Register />} />
-</Routes>
-</BrowserRouter>
-)
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
